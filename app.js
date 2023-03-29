c5e177bdf43f0639fb1f4214a04cbc22672aae29
Tri <16trile@gmail.com>
2023-03-29 14:19:45 -0500

    changed app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // my first commit, no changes here!
+
+let attendance = ["Tri", "Jeremie", "Jeremiah", "Cassie"];
+
+let main = document.getElementsByTagName("main");
+
+for (let i = 0; i < attendance.length; i++) {
+  let p = document.createElement("p");
+  p.innerHTML = `Hello, my name is ${attendance[i]}`;
+  main[0].appendChild(p);
+}
